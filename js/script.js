deb9747a327d69cabb2a6906f3b65ef888459f4a
Vijaykumar Yadav <126179477+Vijay1234667@users.noreply.github.com>
2024-02-09 17:11:17 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// window.addEventListener("load", function () {
+//     setTimeout(
+//         function open(event) {
+//             document.querySelector(".modal").style.
+//                 display = "block";
+//         },
+//         2000
+//     )
+// });
+// document.querySelector(".close").addEventListener
+//     ("click", function () {
+//         document.querySelector(".modal").style.display = "none";
+
+//     });
+
+// $(document).ready(function () {
+//     $("h2").hover(function () {
+//         $(this).css("color", "green");
+//     }, function () {
+//         $(this).css("color", "black");
+//     });
+// });
+
+
+
+function videoconAnimation(){
+
+}
+videoconAnimation()
+gsap.from(".banner-content h1",{
+    y:30,
+    opacity:0,
+    delay:0.5,
+    duration:0.8,
+    stagger:0.2
+})
+
+
+
